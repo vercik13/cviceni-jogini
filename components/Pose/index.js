853a2d524d04b59702b55325440b1c850f9dd9bb
Veronika Vokounová <ver.cik@seznam.cz>
2023-07-06 14:45:17 +0200

    aktualizace

---
+++ components/Pose/index.js
@@ -0,0 +1,8 @@
+export const Pose = (props) => {
+
+  const {src} = props
+
+  return `
+  <img class="pose" src='${src}' />
+  `
+}
--- index.js
+++ index.js
@@ -1,1 +1,50 @@
-console.log('funguju!');
+import { Header } from "./components/Header/index.js";
+import { Intro } from "./components/Intro/index.js";
+import { Pose } from "./components/Pose/index.js";
+
+const appElm = document.querySelector('#app')
+
+const header = {
+  title: 'Jogíni',
+  links: ['domů', 'lekce', 'náš tým', 'události', 'kontakt'],
+}
+
+
+const renderHeader = (items) => {
+  appElm.innerHTML = Header({
+    title: items.title,
+    links: items.links,
+  })
+}
+
+renderHeader(header)
+
+const intro = {
+  heading: 'Vítejte mezi Jogíny',
+  text: `Naše lekce jsou zaměřeny na potřeby klientů, kteří mají odvahu zkusit
+         něco nového. Cvičíme v pomalém tempu s podrobným slovním doprovodem.
+         Postupně se seznámíte se základními principy jógy, jak přístupovat k
+         sobě i ostatním. Krok za krokem objevíte a dostanete pod kontrolu
+         svoje tělo, pocity a emoce.`,
+}
+
+const renderIntro = (items) => {
+  appElm.innerHTML += Intro({
+    heading: items.heading,
+    text: items.text
+  })
+}
+
+renderIntro(intro)
+
+const pose = {
+  src: 'img/yoga-pose.jpg'
+}
+
+const renderPose = (items) => {
+  appElm.innerHTML += Pose({
+    src: items.src
+  })
+}
+
+renderPose(pose)
